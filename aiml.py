50eaeef1e7752f7b0f0457507a14bfaba7eebb9d
pabandasgh <paban1993@gmail.com>
2025-01-13 15:28:42 +0530

    new func

--- aiml.py
+++ aiml.py
@@ -1,8 +1,11 @@
 print("Hello AI ML")
 import numpy as np
 
-array1=np.array(range(1,10))
-array2=np.array(range(1,20))
+array1=np.array(range(10,20))
+array2=np.array(range(1,11))
 print(array1)
 print("\n")
 print(array2)
+
+array3= array1+array2
+print(array3)
